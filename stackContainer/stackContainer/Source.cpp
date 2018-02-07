7662e3f1f8a66d48afe3e68564c910499d8031f5
Diego <36131808+Diegovano@users.noreply.github.com>
2018-02-07 20:24:48 +0000

    Finished basic Stack Container

--- stackContainer/stackContainer/Source.cpp
+++ stackContainer/stackContainer/Source.cpp
@@ -3,15 +3,63 @@
 
 template <class _SType>
 bool stack<_SType>::empty(){
-	
+	if (occSize == 0)return true;
+	return false;
 }
 
 template <class _SType>
 size_t stack<_SType>::size() {
-	return sizeof(this->arr) / sizeof(_SType);
+	return occSize;
+}
+
+template <class _SType>
+void stack<_SType>::pushBack(_SType ent) {
+	if ((occSize + 1) * sizeof(_SType) > 0xFA000) {
+		std::cerr << "Stack cannot contain more elements." << std::endl;
+		system("pause");
+		return;
+	}
+	*lastElem = ent;
+	++lastElem;
+	++occSize;
+}
+
+template <class _SType>
+void stack<_SType>::popBack() {
+	if (occSize == 0) {
+		std::cerr << "Cannot remove element as none exists!" << std::endl;
+		return;
+	}
+	--lastElem;
+	--occSize;
+}
+
+template <class _SType>
+const _SType stack<_SType>::operator[](const size_t ent) {
+	if (ent >= occSize) {
+		std::cerr << "Subscript value out of range." << std::endl;
+		throw;
+	}
+	_SType *tempPtr = arr;
+	for (unsigned int iter = 0; iter < ent; ++iter) {
+		++tempPtr;
+	}
+	return *tempPtr;
 }
 
 int main() {
+	stack<int> intCont;
+	int ent;
+	while (std::cin >> ent) {
+		if (ent == 42)break;
+		intCont.pushBack(ent);
+	}
+	std::cout << intCont[2] << std::endl;
+	std::cout << intCont.size();
+	intCont.popBack();
+	std::cout << " " << intCont.size() << std::endl;
+	intCont.pushBack(128);
+	std::cout << intCont[2] << std::endl;
 	system("pause");
 	return 0;
 }
--- stackContainer/stackContainer/stack.h
+++ stackContainer/stackContainer/stack.h
@@ -4,17 +4,21 @@ template <class _SType>
 class stack {
 private:
 	_SType* arr;
+	_SType* lastElem;
+	size_t occSize;
 public:
 	stack() {
-		if (sizeof(_SType) > 512) {
+		if (sizeof(_SType) > 0xD000) {
 			std::cerr << "The size of your type is too large to make this container have a use.";
-			throw range_error("The size of your type is too large to make this container have a use.");
+			system("pause");
+			return;
 		}
-		arr = new _SType[1024 / sizeof(_SType)];
+		arr = new _SType[0xFA000 / sizeof(_SType)];
+		lastElem = arr;
 	}
 	bool empty();
 	size_t size();
 	void pushBack(_SType ent);
 	void popBack();
-	void popBack(size_t toPop);
+	const _SType operator[](const size_t ent);
 };
