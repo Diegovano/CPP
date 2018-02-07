ee47ae764e990bc4a049bf534380d4beba2263fa
Diego <36131808+Diegovano@users.noreply.github.com>
2018-02-07 23:22:45 +0000

    Added a destructor as well as basic exception handling

--- stackContainer/stackContainer/Source.cpp
+++ stackContainer/stackContainer/Source.cpp
@@ -3,17 +3,20 @@
 
 template <class _SType>
 bool stack<_SType>::empty(){
+	if (!isValid)throw std::runtime_error("Cannot use member functions of an invalid Stack!");
 	if (occSize == 0)return true;
 	return false;
 }
 
 template <class _SType>
 size_t stack<_SType>::size() {
+	if (!isValid)throw std::runtime_error("Cannot use member functions of an invalid Stack!");
 	return occSize;
 }
 
 template <class _SType>
 void stack<_SType>::pushBack(_SType ent) {
+	if (!isValid)throw std::runtime_error("Cannot use member functions of an invalid Stack!");
 	if ((occSize + 1) * sizeof(_SType) > 0xFA000) {
 		std::cerr << "Stack cannot contain more elements." << std::endl;
 		system("pause");
@@ -26,6 +29,7 @@ void stack<_SType>::pushBack(_SType ent) {
 
 template <class _SType>
 void stack<_SType>::popBack() {
+	if (!isValid)throw std::runtime_error("Cannot use member functions of an invalid Stack!");
 	if (occSize == 0) {
 		std::cerr << "Cannot remove element as none exists!" << std::endl;
 		return;
@@ -36,9 +40,10 @@ void stack<_SType>::popBack() {
 
 template <class _SType>
 const _SType stack<_SType>::operator[](const size_t ent) {
+	if (!isValid)throw std::runtime_error("Cannot use member functions of an invalid Stack!");
 	if (ent >= occSize) {
 		std::cerr << "Subscript value out of range." << std::endl;
-		throw;
+		return _SType();
 	}
 	_SType *tempPtr = arr;
 	for (unsigned int iter = 0; iter < ent; ++iter) {
@@ -47,19 +52,33 @@ const _SType stack<_SType>::operator[](const size_t ent) {
 	return *tempPtr;
 }
 
+struct bigThing {
+	char vBig[300000];
+};
+
 int main() {
-	stack<int> intCont;
-	int ent;
-	while (std::cin >> ent) {
-		if (ent == 42)break;
-		intCont.pushBack(ent);
+	try {
+		bigThing vvBigTing;
+		stack<int> intCont;
+		int ent;
+		while (std::cin >> ent) {
+			if (ent == 42)break;
+			intCont.pushBack(ent);
+		}
+		std::cout << intCont[2] << std::endl;
+		std::cout << intCont.size();
+		intCont.popBack();
+		std::cout << " " << intCont.size() << std::endl;
+		intCont.pushBack(128);
+		std::cout << intCont[2] << std::endl;
+		stack<bigThing> vBigTing;
+		vBigTing.pushBack(vvBigTing);
+		system("pause");
+		return 0;
 	}
-	std::cout << intCont[2] << std::endl;
-	std::cout << intCont.size();
-	intCont.popBack();
-	std::cout << " " << intCont.size() << std::endl;
-	intCont.pushBack(128);
-	std::cout << intCont[2] << std::endl;
-	system("pause");
-	return 0;
+	catch (std::runtime_error& e) {
+		std::cout << e.what() << std::endl;
+		system("pause");
+		return -1;
+	}
 }
--- stackContainer/stackContainer/stack.h
+++ stackContainer/stackContainer/stack.h
@@ -6,15 +6,21 @@ private:
 	_SType* arr;
 	_SType* lastElem;
 	size_t occSize;
+	bool isValid = false;
 public:
 	stack() {
 		if (sizeof(_SType) > 0xD000) {
-			std::cerr << "The size of your type is too large to make this container have a use.";
+			std::cerr << "The size of your type is too large to make this container have a use." << std::endl;
 			system("pause");
 			return;
 		}
 		arr = new _SType[0xFA000 / sizeof(_SType)];
 		lastElem = arr;
+		isValid = true;
+	}
+	~stack() {
+		delete arr, lastElem;
+		isValid = false;
 	}
 	bool empty();
 	size_t size();
